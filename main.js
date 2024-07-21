330e147b381104edb2ae6f791e56677858b2f4ea
Abel Koech <koechabel161@gmail.com>
2024-07-21 17:59:48 +0300

    liker

--- main.js
+++ main.js
@@ -6,6 +6,42 @@ const FULL_HEART = '♥'
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  const errorModal = document.getElementById('modal');
+  const hearts = document.querySelectorAll('.like-glyph')
+  errorModal.classList.add('hidden')
+  hearts.forEach((heart)=>{
+    heart.addEventListener('click',function(){
+      mimicServerCall()
+    .then(()=>{
+      heart.classList.add('activated-heart')
+      heart.innerHTML=`♥`
+    })
+  }).catch((Error)=>{
+    const errorMessage=document.getElementById('modal-message')
+    errorModal.classList.remove('hidden')
+    errorMessage.innerText='Random server error. Try again.'
+    setTimeout(() => {
+      errorModal.classList.add('hidden')
+    },3000);
+  
+  
+    })
+    })
+    })
+    herts.forEach((heart)=>{
+      heart.addEventListener('click',function(){
+      if(heart.classList.contain('activated-heart')){
+        heart.classList.remove('activated-heart')
+        heart.innerHTML='♡'
+
+      }
+      })
+    })
+  
+ 
+
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
